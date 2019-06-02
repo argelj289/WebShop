8983bbae7933c57ba2ecaad6ae05746dab5cca24
argelj289 <argelj04@gmail.com>
2019-06-03 00:21:13 +0800

    implemented routing on angular

--- webpage/src/app/app.module.ts
+++ webpage/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { NotFoundComponent } from './error-page/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
